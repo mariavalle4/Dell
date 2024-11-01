d2f74a63ea47ddd96515e8a6207eaa66b5e1151f
Maria Eduard Valle <164665826+mariavalle4@users.noreply.github.com>
2024-11-01 17:58:21 -0300

    vs code adicionado

--- ASPNET/Aula03/01_ViewExploration/obj/Debug/net8.0/01_ViewExploration.AssemblyInfo.cs
+++ ASPNET/Aula03/01_ViewExploration/obj/Debug/net8.0/01_ViewExploration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_ViewExploration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6dc7e2f3496fc53f53d6977bb4facf4e2ce81f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_ViewExploration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_ViewExploration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
