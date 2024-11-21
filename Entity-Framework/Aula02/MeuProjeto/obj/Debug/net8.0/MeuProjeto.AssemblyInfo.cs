78ef13f98e708a9d86df70942cd4c1067525dd04
Maria Eduard Valle <164665826+mariavalle4@users.noreply.github.com>
2024-11-21 14:29:51 -0300

    a

--- Entity-Framework/Aula02/MeuProjeto/obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
+++ Entity-Framework/Aula02/MeuProjeto/obj/Debug/net8.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f901b95c0eafe9ca538dcf5b528ae4baf2ae268")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40df007b87e9d61d973f2e6fe4c9a4c93434e881")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
